fc0e19c46099601922853bc9f49467e6b551a66e
twinks14 <svnsavani0@gmail.com>
2017-06-08 21:09:38 +0530

    add file permit

--- app/controllers/foot_traffic_analysis_controller.rb
+++ app/controllers/foot_traffic_analysis_controller.rb
@@ -1,11 +1,11 @@
 class FootTrafficAnalysisController < ApplicationController
   def index
-    @output = process_file if params[:file].present?
+    @output = process_file if file_params[:file].present?
   end
 
   def run
     if file_present? && log_file?
-      redirect_to root_path(file: params[:file].path)
+      redirect_to root_path(file: file_params[:file].path)
     else
       redirect_to root_path, flash: { error: 'Something wrong, Please try after sometime or Please upload correct file' }
     end
@@ -15,7 +15,7 @@ class FootTrafficAnalysisController < ApplicationController
 
   def process_file
     begin
-      traffic_analysis = FootTrafficAnalysis.new(LogFile.new(params[:file]))
+      traffic_analysis = FootTrafficAnalysis.new(LogFile.new(file_params[:file]))
       flash.now[:success] = 'Successfully parsed'
       traffic_analysis.generate_report
     rescue StandardError => e
@@ -25,12 +25,16 @@ class FootTrafficAnalysisController < ApplicationController
 
   # check if file is present or not?
   def file_present?
-    params[:file].present?
+    file_params[:file].present?
   end
 
   # allow only '.log' file,
   # In order to support CSV file: add Content-Type: text/csv
   def log_file?
-    params[:file].content_type == 'application/octet-stream'
+    file_params[:file].content_type == 'application/octet-stream'
+  end
+
+  def file_params
+    params.permit(:file)
   end
 end
